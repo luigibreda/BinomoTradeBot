b2a527bae7d119a929097e9b76e65af4a7af6c77
half144 <guilhermefeller2002@gmail.com>
2023-04-10 12:45:07 -0300

    update

--- binomo-extension-client/src/libs/socket.ts
+++ binomo-extension-client/src/libs/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000");
+export const socket = io("https://binomotradebot-production.up.railway.app/");
 
 socket.on("connect", () => {
   console.log("Connected to server");
