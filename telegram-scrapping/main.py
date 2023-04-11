e5dabc71af6c9eb7dbf743efabb0f09d03832b4d
luigibreda <luigimatheus@hotmail.com>
2023-04-11 11:48:57 -0300

    melhorias no script

--- telegram-scrapping/main.py
+++ telegram-scrapping/main.py
@@ -1,61 +1,64 @@
-import logging
+
+
+import telegram
 import asyncio
-import json
 import aiohttp
-from telethon import TelegramClient, events
+import json
+import logging
 
-# Configure as variáveis abaixo com suas credenciais do Telegram
-api_id = 18414817
-api_hash = '76a4b4fc17923a688267b9fd55009953'
-phone_number = '+5547936189205'  # Seu número de telefone, incluindo código do país
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Configure a sessão abaixo com o nome da sessão que você escolheu anteriormente
-session_name = 'session_name'
+# Configure a API key abaixo com a sua chave do BotFather
+bot_token = '6218389548:AAHZdie21j_A9GlBkMRTXfRIIFdBL2cLpOQ'
 
 # Configure o webhook abaixo com a URL do seu webhook
 webhook_url = 'https://binomotradebot-production.up.railway.app/webhook'
 
-# Configurar o logger
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 async def main():
-    async with TelegramClient(session_name, api_id, api_hash) as client:
-        await client.connect()
-        if not await client.is_user_authorized():
-            try:
-                await client.send_code_request(phone_number)
-                await client.sign_in(phone_number, input('Digite o código enviado: '))
-            except Exception as e:
-                logger.exception(f'Não foi possível fazer o login: {str(e)}')
-                return
-        logger.info('Você está logado no Telegram como %s', (await client.get_me()).username)
+    bot = telegram.Bot(token=bot_token)
+    logger.info('Você está logado no Telegram')
 
-        # Obter todos os grupos
-        async for dialog in client.iter_dialogs():
-            if dialog.is_group:
-                logger.info('Grupo encontrado: %s, ID: %d', dialog.name, dialog.id)
+    bot_info = await bot.get_me()
 
-        # Obter o grupo desejado
-        group_entity = await client.get_entity(-814777352)
+    logger.info('Bot conectado: %s (username: %s, ID: %d)', bot_info.first_name, bot_info.username, bot_info.id)
+    logger.info('Bot pode ser encontrado em: t.me/%s', bot_info.username)
 
-        # Receber e enviar somente as novas mensagens do grupo
-        @client.on(events.NewMessage(chats=group_entity))
-        async def handler(event):
-            logger.info('Nova mensagem recebida: %s', event.message.text)
-            payload = json.dumps({'emit': 'testeluigi', 'data': event.message.text})
-            try:
-                async with aiohttp.ClientSession() as session:
-                    async with session.post(webhook_url, data=payload, headers={'Content-Type': 'application/json'}) as response:
-                        if response.status != 200:
-                            logger.warning('Erro ao enviar mensagem para o webhook: %d %s', response.status, response.reason)
-            except Exception as e:
-                logger.exception(f'Não foi possível enviar mensagem para o webhook: {str(e)}')
-                
-            logger.info('Mensagem enviada com sucesso ao webhook: %s', event.message.text)
+    # Obter as últimas atualizações
+    updates = await bot.get_updates()
+
+    # Percorrer as atualizações e extrair o chat ID de cada mensagem
+    chat_ids = set()
+    for update in updates:
+        chat = update.effective_chat
+        chat_id = chat.id
+        chat_ids.add(chat_id)
+
+    logger.info('Chats disponíveis: %s', chat_ids)
+
+    # Obter o grupo desejado
+    group_id = -814777352  # Insira o ID do grupo desejado
+    group_entity = telegram.Chat(id=group_id, type='group')
+
+    # Receber e enviar somente as novas mensagens do grupo
+    offset = None
+    while True:
+        updates = await bot.get_updates(offset=offset, timeout=30)
+        for update in updates:
+            offset = update.update_id + 1
+            if update.message.chat_id == group_id:
+                logger.info('Nova mensagem recebida: %s', update.message.text)
+                payload = json.dumps({'emit': 'testeluigi', 'data': update.message.text})
+                try:
+                    async with aiohttp.ClientSession() as session:
+                        async with session.post(webhook_url, data=payload, headers={'Content-Type': 'application/json'}) as response:
+                            if response.status != 200:
+                                logger.warning('Erro ao enviar mensagem para o webhook: %d %s', response.status, response.reason)
+                except Exception as e:
+                    logger.exception(f'Não foi possível enviar mensagem para o webhook: {str(e)}')
+                    
+                logger.info('Mensagem enviada com sucesso ao webhook: %s', update.message.text)
         
-        # Executar o cliente em segundo plano
-        await client.run_until_disconnected()
+        await asyncio.sleep(1)
 
-if __name__ == '__main__':
-    asyncio.run(main())
+asyncio.run(main())
