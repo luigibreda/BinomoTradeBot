456a611672a3d4b0f25fec1175cd2a1277b7ff30
luigibreda <luigimatheus@hotmail.com>
2023-04-11 19:34:46 -0300

    troca de mercado em construção

--- binomo-extension-client/public/content.js
+++ binomo-extension-client/public/content.js
@@ -39,6 +39,30 @@ const actions = {
       button.click();
     }, 500);
   },
+  EUR_USD: () => {
+    console.log("iniciou")
+    const menuItems = document.querySelectorAll('#qa_trading_assetDialog > lib-platform-scroll > div > div > section > div'); // seleciona todos os itens do menu
+    let itemToClick = null;
+    
+    // varre os itens do menu e encontra o item que contém o texto desejado
+    for (let i = 0; i < menuItems.length; i++) {
+      console.log(menuItems[i].textContent)
+      if (menuItems[i].textContent === 'EUR/USD') {
+        itemToClick = menuItems[i]; // atribui o item a ser clicado à variável
+        break; // interrompe o loop assim que encontrar o item desejado
+      }
+    }
+    
+    if (itemToClick) {
+      itemToClick.click(); // clica no item desejado
+    }
+  },
+  ADA_USD: () => {
+    console.log('Abre o menu de ativos')
+    document.querySelector("#asset-0 > button").click()
+    console.log('Cica no ativo ADA/USD')
+    document.querySelector("#qa_trading_assetDialog > lib-platform-scroll > div > div > section > div:nth-child(4)").click()
+  },
 };
 
 const performAction = (action) => {
@@ -50,6 +74,11 @@ const performAction = (action) => {
     console.log("Error in content.js", error);
   }
 };
+
+
+setInterval(() => {
+  performAction("EUR_USD");
+}, 3000);
 
 chrome.runtime.onMessage.addListener((request, sender) => {
   performAction(request.type);
