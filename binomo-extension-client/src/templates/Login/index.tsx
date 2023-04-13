2d36071cce0ac8d39fdb1670e16bdfc4a5441e69
half144 <guilhermefeller2002@gmail.com>
2023-04-13 10:53:21 -0300

    useLogin custom hook

--- binomo-extension-client/src/templates/Login/index.tsx
+++ binomo-extension-client/src/templates/Login/index.tsx
@@ -1,28 +1,13 @@
 import { useForm } from "react-hook-form";
 import { ActionButton } from "../../components/ActionButton";
 import { Input } from "../../components/Inputs";
-import { useAuth } from "../../contexts/AuthContext";
-import { useState } from "react";
+import { useLogin } from "./hooks/useLogin";
 
 export const Login = () => {
-  const [error, setError] = useState<string | null>(null);
-  const { register, handleSubmit } = useForm();
-  const { login } = useAuth();
-
-  const onSubmit = async (data: any) => {
-    setError(null);
-    try {
-      await login(data);
-    } catch (error: any) {
-      setError(error.message);
-    }
-  };
+  const { error, handleLogin, register } = useLogin();
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex pt-8 flex-col gap-2"
-    >
+    <form onSubmit={handleLogin} className="flex pt-8 flex-col gap-2">
       <Input
         label="Nome de usuário"
         placeholder="Usuário"
