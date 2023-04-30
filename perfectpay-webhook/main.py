d21b42fe3467f835f486935fa5a88ae61819bf26
Luigi Breda <luigimatheus@hotmail.com>
2023-04-30 23:33:01 +0000

    funcional e log melhorado

--- perfectpay-webhook/main.py
+++ perfectpay-webhook/main.py
@@ -1,16 +1,28 @@
-import os
 import requests
 from flask import Flask, request
+import logging
 
+logging.getLogger('werkzeug').disabled = True
 app = Flask(__name__)
 
 API_URL = 'https://binomotradebot-production.up.railway.app/api/auth/register'
+
+# Configura o logger
+logging.basicConfig(
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO,
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler('app.log')
+    ]
+)
 
 @app.route('/', methods=['POST'])
 def webhook():
     json_data = request.json
     customer_email = json_data['customer']['email']
-    print(f'Email do cliente: {customer_email}')
+    logging.info(f'Webhook recebido para o email {customer_email}')
     
     # Cria o payload com os dados do usuário
     username = customer_email.split('@')[0]  # Usa o email do cliente como nome de usuário
@@ -25,12 +37,11 @@ def webhook():
     # Envia a requisição POST para criar o usuário
     response = requests.post(API_URL, json=payload)
     if response.status_code == 201:
-        print(f'Usuário criado com sucesso: {username}')
+        logging.info(f'Usuário criado com sucesso para o email {customer_email}')
     else:
-        print(f'Erro ao criar usuário: {response.content}')
+        logging.error(f'Erro ao criar usuário para o email {customer_email}: {response.content}')
     
     return 'Webhook recebido com sucesso!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=80)
