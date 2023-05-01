21561409514bc5caa8b53b9e2abc2c2c0625d8bd
Luigi Breda <luigimatheus@hotmail.com>
2023-05-01 01:37:28 +0000

    br

--- perfectpay-webhook/main.py
+++ perfectpay-webhook/main.py
@@ -8,4 +8,5 @@ def handle_post():
     return 'OK'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
