d80f6a839e672ad116a50649b72259f50f573591
Luigi Breda <luigimatheus@hotmail.com>
2023-05-01 01:03:32 +0000

    ajuda Deus

--- perfectpay-webhook/main copy.py
+++ perfectpay-webhook/main copy.py
@@ -7,8 +7,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-logging.getLogger('werkzeug').disabled = False
-app = Flask(__name__)
+logging.getLogger('werkzeug').disabled = True
+# app = Flask(__name__)
 
 API_URL = 'https://binomotradebot-production.up.railway.app/api/auth/register'
 
@@ -116,5 +116,5 @@ def webhook():
     
     return 'Webhook recebido com sucesso!'
 
-app.run(host='0.0.0.0', port=80)
+app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
  
