0aedc0591011c9b89eb49d54f1286bc43888fb07
Luigi Breda <luigimatheus@hotmail.com>
2023-05-01 02:36:21 +0000

    teste

---
+++ perfectpay-webhook/app.mjs
@@ -0,0 +1,52 @@
+import { createServer } from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/', async (req, res) => {
+  
+  const { customer } = req.body;
+  const customerEmail = customer.email;
+
+  console.log(`Webhook recebido para o email ${customerEmail}`);
+
+  // Cria o payload com os dados do usuário
+  const username = customerEmail.split('@')[0]; // Usa o email do cliente como nome de usuário
+  const password = 'Senha123Padrao'; // Define uma senha padrão
+  const payload = {
+    username,
+    password,
+    plan: '2',
+    expiresIn: 30,
+  };
+
+  // Envia a requisição POST para criar o usuário
+  try {
+    const response = await fetch('https://binomotradebot-production.up.railway.app/api/auth/register', {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.status === 201) {
+      console.log(`Usuário criado com sucesso para o email: ${customerEmail} - Login: ${username} e Senha: ${password}`);
+    } else {
+      console.error(`Erro ao criar usuário para o email ${customerEmail}`);
+    }
+  } catch (error) {
+    console.error(`Erro ao criar usuário para o email ${customerEmail}: ${error}`);
+  }
+
+  res.send('Webhook recebido com sucesso!');
+});
+
+const server = createServer(app);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
--- perfectpay-webhook/main.py
+++ perfectpay-webhook/main.py
@@ -8,17 +8,49 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
+logging.getLogger('werkzeug').disabled = True
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def handle_post():
+API_URL = 'https://binomotradebot-production.up.railway.app/api/auth/register'
 
+# Configura o logger
+logging.basicConfig(
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO,
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler('app.log')
+    ]
+)
+
+@app.route('/', methods=['GET', 'POST'])
+def webhook():
     json_data = request.json
     customer_email = json_data['customer']['email']
-    print('Received a POST request!')
-    print(customer_email)
-    return 'ok2'
+    logging.info(f'Webhook recebido para o email {customer_email}')
+    
+    # Cria o payload com os dados do usuário
+    username = customer_email.split('@')[0]  # Usa o email do cliente como nome de usuário
+    password = 'Senha123Padrao'  # Define uma senha padrão
+    payload = {
+        'username': username,
+        'password': password,
+        'plan': '2',
+        'expiresIn': 30
+    }
+    
+    # Envia a requisição POST para criar o usuário
+    response = requests.post(API_URL, json=payload)
+    if response.status_code == 201:
+        logging.info(f'Usuário criado com sucesso para o email: {customer_email} - Login: {username} e Senha: {password}')
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port)
+    else:
+        logging.error(f'Erro ao criar usuário para o email {customer_email}')
+    
+    return 'Webhook recebido com sucesso!'
+
+# app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
+
+port = int(os.environ.get('PORT', 80))
+app.run(host='0.0.0.0', port=port)
