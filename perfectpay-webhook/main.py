0d6633f22c8dfe67f236efe8545c58bc0715a2ac
Luigi Breda <luigimatheus@hotmail.com>
2023-05-01 00:29:56 +0000

    testee

--- perfectpay-webhook/main.py
+++ perfectpay-webhook/main.py
@@ -7,7 +7,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-logging.getLogger('werkzeug').disabled = False
+logging.getLogger('werkzeug').disabled = True
 app = Flask(__name__)
 
 API_URL = 'https://binomotradebot-production.up.railway.app/api/auth/register'
