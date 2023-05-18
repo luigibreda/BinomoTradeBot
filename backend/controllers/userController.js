0e9fe5375d641446ec0bb4a4f3b847a168303ca6
Luigi Breda <luigimatheus@hotmail.com>
2023-05-18 08:43:23 -0300

    endpoint remove and update added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import { User } from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import moment from 'moment';
 
 const genHashedString = async (string) => {
   const salt = await bcrypt.genSalt(10);
@@ -89,5 +90,47 @@ export const me = async (req, res) => {
     res.status(200).json(rest);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const { email } = req.body.customer;
+    const username = email.split('@')[0];
+
+    const user = await User.findOneAndDelete({ username: username });
+
+    if (user) {
+      return res.status(200).json({ message: "User deleted" });
+    } else {
+      return res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const { email } = req.body.customer;
+    const username = email.split('@')[0];
+
+    const user = await User.findOne({ username: username });
+
+    if (user) {
+      const newExpiresAt = moment().add(30, 'days');
+      
+      await User.findOneAndUpdate(
+        { username: username },
+        { expiresAt: newExpiresAt },
+        { new: true }
+      );
+
+      return res.status(200).json({ message: "User updated" });
+    } else {
+      return res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, register, me } from "../controllers/userController.js";
+import { login, register, me, remove, update } from "../controllers/userController.js";
 import { authGuard } from "../middlewares/authGuard.js";
 
 const router = Router();
@@ -7,5 +7,7 @@ const router = Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", authGuard, me);
+router.post("/remove", remove);
+router.post("/update", update);
 
 export default router;
